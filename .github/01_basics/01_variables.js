9325b5a839533366d43a4001670547bd202f55ab
developer-shriraj <shriraj_bhore@yahoo.com>
2024-06-06 15:09:24 +0000

    data types & variables

---
+++ .github/01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let  accountEmail = "shriraj_bhore@yahoo.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
