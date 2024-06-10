24e4e13e30e07a1bfd873efa50d08f8b0232bb10
developer-shriraj <shriraj_bhore@yahoo.com>
2024-06-10 13:38:15 +0000

    Comparison & Conversion Operations

--- .github/01_basics/03_conversionOperation.js
+++ .github/01_basics/03_conversionOperation.js
@@ -1,9 +1,39 @@
-let score = "33abc"
+//  let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// // **********************Operations***********************
+// let value = 3
+// let negValue = -value
+// console.log(negValue); 
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+
+// let str1 = "Shriraj"
+// let str2 = " Bhore"
+
+// let merge = str1+str2
+
+// console.log(merge);
+
+// console.log(+true);
+// console.log(+"");
+
+
+
+// let num1, num2, num3 
+
+// num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
